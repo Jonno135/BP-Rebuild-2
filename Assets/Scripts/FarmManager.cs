292fcc300bfcea3dee4a61061a9a65c994271caf
badfrienderic <theericota@gmail.com>
2018-05-04 13:55:48 -0700

    first commit to farm branch

---
+++ Assets/Scripts/FarmManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FarmManager : MonoBehaviour {
+
+	[SerializeField]
+	private Crop cropA1;
+	[SerializeField]
+	private Crop cropA2;
+	[SerializeField]
+	private Crop cropA3;
+	[SerializeField]
+	private Crop cropA4;
+	[SerializeField]
+	private Crop cropB1;
+	[SerializeField]
+	private Crop cropB2;
+	[SerializeField]
+	private Crop cropB3;
+	[SerializeField]
+	private Crop cropB4;
+
+
+	private Crop[] cropList;
+	private int numCrops = 8;
+
+	// Use this for initialization
+	void Start () {
+		initCropList ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		displayCropStatus ();
+	}
+
+	public void plant(Crop crop){
+		crop.status = 1;
+	}
+
+	public void grow(Crop crop){
+		crop.status = 2;
+	}
+
+	public void harvest(Crop crop){
+		crop.status = 0;
+	}
+
+	public Crop[] getEmptyCrops(){
+		Crop[] openCrops = new Crop[numCrops];
+		int counter = 0;
+		foreach (Crop a in cropList){
+			if (a.status == 0) {
+				openCrops [counter] = a;
+				counter++;
+			}
+		}
+		return openCrops;
+	}
+
+	public Crop[] getHarvestableCrops(){
+		Crop[] harvestableCrops = new Crop[numCrops];
+		int counter = 0;
+		foreach (Crop a in cropList){
+			if (a.status == 2) {
+				harvestableCrops [counter] = a;
+				counter++;
+			}
+		}
+		return harvestableCrops;
+	}
+
+	public Crop[] getCropList(){
+		return cropList;
+	}
+
+	private void displayCropStatus(){
+		//print (cropList);
+		foreach (Crop c in cropList) {
+			c.cropTxt.text = "Status: " + c.status;
+		}
+	}
+
+	private void initCropList(){
+		cropList = new Crop[numCrops];
+		cropList [0] = cropA1;
+		cropList [1] = cropA2;
+		cropList [2] = cropA3;
+		cropList [3] = cropA4;
+		cropList [4] = cropB1;
+		cropList [5] = cropB2;
+		cropList [6] = cropB3;
+		cropList [7] = cropB4;
+	}
+}
---
+++ Assets/Scripts/FarmWorker1.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmWorker1 : MonoBehaviour {
+
+	[SerializeField]
+	private FarmManager farm;
+	private Rigidbody2D farmWorker;
+	public Transform[] target;
+
+	private Vector2 dropOffPos = new Vector2(25,25);
+
+	//Stats
+	private float speed;
+	private int maxCapacity;
+	private int numPotatos;
+
+	// Use this for initialization
+	void Start () {
+		farm.getCropList ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		act (determineStatus ());
+	}
+
+	private void plant(Crop crop){
+		farm.plant(findNearestEmptyCrop());
+	}
+
+	private void harvest(Crop crop){
+		//findNearestHarvestableCrop
+		//farm.harvest(crop)
+		//potatoCount++;
+	}
+
+	private void idle(){
+		//moveToIdleSpot
+		//wait?
+	}
+
+	private void dropOff(){
+		moveTo (dropOffPos);
+		//while not there ^ ?
+		//then
+		//farm.dropOff(potatoCount);
+		//potatoCount = 0;
+	}
+
+	private int determineStatus(){
+		/*
+		 * if (theres a crop with status 2 AND worker !atCapacity)
+		 * status = 1; //pick crop
+		 * else if (theres a crop with status 0 AND worker !atCapacity)
+		 * status = 2; //plant crop
+		 * else if (!atCapacity)
+		 * status = 0; //idle
+		 * else if (atCapacity)
+		 * status = 3; //drop off
+		 */
+		int sts = -1;
+		if(!atCapacity() && farm.getHarvestableCrops().Length != 0){
+			sts = 1; //pickCrop
+		} else if (!atCapacity() && (farm.getEmptyCrops().Length != 0)) {
+			sts = 2; //plantCrop
+		} else if (!atCapacity()) {
+			sts = 0; //idle
+		} else if (atCapacity()) {
+			sts = 3; //drop off
+		}
+		return sts;
+	}
+
+	private void act(int status){
+		switch(status){
+		case 1:
+			//harvest (crop);
+			break;
+		case 2:
+			//plant(crop);
+			break;
+		case 0:
+			//idle()
+			break;
+		case 3:
+			//dropOff()
+			break;
+		}
+	}
+
+	private Crop findNearestEmptyCrop(){
+		Crop[] emptyCrops = farm.getEmptyCrops ();
+		float shortestDistance = Mathf.Infinity;
+		Crop nearestCrop;
+		if(emptyCrops != null){
+			foreach (Crop c in emptyCrops) {
+				float dist = getDistanceTo (c.transform.position);
+				if( dist < shortestDistance) {
+					nearestCrop = c;
+				}
+			}
+		}
+		return nearestCrop;
+	}
+
+	private void findNearestHarvestableCrop(){
+		//for each empty crop
+		//calculate the minimum distance
+		//return that crop
+	}
+
+	private void moveTo(Vector2 destination){
+		if ((Vector2)farmWorker.transform.position != destination) {
+			Vector2 pos = Vector2.MoveTowards (destination, destination, speed * Time.deltaTime);
+			farmWorker.transform.position = pos;
+		}
+	}
+
+	private float getDistanceTo(Vector2 destination){
+		return Vector2.Distance (farmWorker.transform.position, destination);
+	}
+
+	private bool atCapacity(){
+		if (numPotatos < maxCapacity) {
+			return false;
+		}
+		return true;
+	}
+
+}
